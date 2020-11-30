ab2e66663cb88fce513de016802b985989eea06b
arkeo01 <kishor.1@iitj.ac.in>
2020-11-30 14:33:45 +0530

    Database integrated, first version of frontend!

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -30,10 +30,11 @@ class Signin extends React.Component {
         })
             .then(response => response.json())
             .then(user => {
-                if(user){
+                if(user.id){
                     this.props.loadUser(user);
                     this.props.onRouteChange('home');
                 }
+                // TODO: Add error message for wrong signin/nonexistent user
             })
     }
 
